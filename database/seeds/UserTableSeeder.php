72f117fb5c7ec076b6f1ef7ac274592bf892c7c8
wvillatoro <wcvillatoroc@gmail.com>
2015-11-10 21:29:58 +0000

    Add user admin in table Users

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
     use Illuminate\Database\Seeder;
+    use TeachMe\Entities\User;
 
     class UserTableSeeder extends Seeder 
     {
@@ -11,10 +12,12 @@
         
         private function createAdmin()
         {
-            
-            
+            User::create([
+                'first_name'=>'Willy',
+                'last_name'=>'Villatoro',
+                'email'=>'willy.villatoro@onsec.gob.gt',
+                'password'=>bcrypt('Admin')
+            ]);
         }
-    
-        
     }
 
