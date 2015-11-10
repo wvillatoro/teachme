3193ef6963d961593d913b452fce68b40dafe917
wvillatoro <wcvillatoroc@gmail.com>
2015-11-10 17:11:44 +0000

    Ticket and TicketVote new model and migration

--- database/migrations/2015_11_10_154608_create_tickets_table.php
+++ database/migrations/2015_11_10_154608_create_tickets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateTicketsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,15 +12,19 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('tickets', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('first_name');
-			$table->string('last_name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->string('active')->default(true);
-			$table->rememberToken();
+			
+			$table->string('title', 200);
+			$table->enum('status', ['open', 'closed']);
+			
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			
+			
+			
+			
 			$table->timestamps();
 		});
 	}
@@ -32,7 +36,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('tickets');
 	}
 
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+    use Illuminate\Database\Seeder;
+
+    class UserTableSeeder extends Seeder 
+    {
+        public function run() 
+        {
+            $this->createAdmin();
+        }
+        
+        private function createAdmin()
+        {
+            
+            
+        }
+    
+        
+    }
+
